f7beaa3551d261663e7869c92e6d5202e4a5ee5d
omarsalem7 <omarsalem0721@gmail.com>
2022-06-14 22:58:35 +0200

    Fix linetrs and commit my configuration

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,6 +1,6 @@
 class Api::CoursesController < ApplicationController
   def index
-      courses = Course.order(created_at: :DESC)
+    courses = Course.order(created_at: :DESC)
     render json: courses
   end
 end
