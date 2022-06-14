a08b6b3676379d1824741d01493a54d60b80ecf7
zairdon20 <josphat2020@gmail.com>
2022-06-14 02:48:56 -0500

    Create validation for models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   has_many :reservations, dependent: :destroy
+
+  validates :name, :description, :size, :location, :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
+
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true
 end
