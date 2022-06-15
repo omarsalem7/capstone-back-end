cc39e1143a32239dbaf131a5fe014a81795ea9f8
zairdon20 <josphat2020@gmail.com>
2022-06-15 05:54:08 -0500

    Update the routes for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :courses
-    resources :reservation
+    resources :reservations
   end
 end
