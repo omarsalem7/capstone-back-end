72284755a2464d735c958024874eca8badab9d09
zairdon20 <josphat2020@gmail.com>
2022-06-15 05:43:36 -0500

    Remove commented code

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -2,7 +2,6 @@ class Api::ReservationsController < ApplicationController
    def index 
     reservations = Reservation.all
     render json: reservations
-    # change this line of code
    end
 end
 
