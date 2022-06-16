90aef3325d4559448d14291601c2606ef973f540
zairdon20 <josphat2020@gmail.com>
2022-06-16 08:30:45 -0500

    Add the reservations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
-  validates :email, uniqueness: true, presence: true
+  # validates :email, uniqueness: true, presence: true
   validates :name, presence: true
 end
