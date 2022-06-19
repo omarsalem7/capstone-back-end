e4479dfd4700d99cccc60d841c1c7e7d68acbcb7
zairdon20 <josphat2020@gmail.com>
2022-06-19 14:23:13 -0500

    Add reservation store

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -3,5 +3,20 @@ class Api::ReservationsController < ApplicationController
     reservations = Reservation.all
     render json: reservations
    end
+
+   def create
+      reservation = Reservation.new(reservation_params)
+      if reservation.save
+         render json: reservation, status: 200
+      else
+         render json: {errors: "Reservations were not created"}, status: :unprocessible_entity
+      end
+   end
+
+   private
+
+   def reservation_params
+      params.require(:reservation).permit(:date, :city, :course_id, :user_id)
+   end
 end
 
