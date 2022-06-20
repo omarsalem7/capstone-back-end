fbdd5b72f3b5e8c72edbfa33ebfce7b717a69a7d
sneeu-leeu <sa070197@gmail.com>
2022-06-20 18:22:58 +0200

    Feature & Fix: update parameters, serialiser and session controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,47 +1,17 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionFix
   respond_to :json
-
   private
-
   def respond_with(resource, _opts = {})
     if resource.persisted?
       render json: {
-        status: {code: 200, message: 'Signed up successfully.'},
+        status: { code: 200, message: 'Signed up sucessfully.' },
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
       }
     else
       render json: {
-        status: {message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+        status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
       }, status: :unprocessable_entity
-    end
-  end
-end
-
-class Users::SessionsController < Devise::SessionsController
-  respond_to :json
-  private
-
-  def respond_with(resource, _opts = {})
-    render json: {
-      status: {code: 200, message: 'Logged in successfully.'},
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-    }, status: :ok
-  end
-
-  def respond_to_on_destroy
-    if current_user
-      render json: {
-        status: 200,
-        message: "logged out successfully"
-      }, status: :ok
-    else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
     end
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :created_at
+  attribute :created_date do |user|
+    user&.created_at&.strftime('%d/%m/%Y')
+  end
 end
