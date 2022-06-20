4592225b10870b78242565224739b78d07cc5e58
sneeu-leeu <sa070197@gmail.com>
2022-06-20 14:32:42 +0200

    Feature and Config: update migrations and config localhost

--- db/migrate/20220613132548_create_user.rb
+++ db/migrate/20220613132548_create_user.rb
@@ -2,7 +2,7 @@ class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :role
+      t.boolean :admin, default: false
 
       t.timestamps
     end
