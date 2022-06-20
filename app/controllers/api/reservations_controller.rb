44b8e23255ea1969d71f570cd2207b94cdf752c2
zairdon20 <josphat2020@gmail.com>
2022-06-20 06:06:42 -0500

    Fix linter errors

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,22 +1,21 @@
 class Api::ReservationsController < ApplicationController
-   def index 
+  def index
     reservations = Reservation.all
     render json: reservations
-   end
+  end
 
-   def create
-      reservation = Reservation.new(reservation_params)
-      if reservation.save
-         render json: reservation, status: 200
-      else
-         render json: {errors: "Reservations were not created"}, status: :unprocessible_entity
-      end
-   end
+  def create
+    reservation = Reservation.new(reservation_params)
+    if reservation.save
+      render json: reservation, status: 200
+    else
+      render json: { errors: 'Reservations were not created' }, status: :unprocessible_entity
+    end
+  end
 
-   private
+  private
 
-   def reservation_params
-      params.require(:reservation).permit(:date, :city, :course_id, :user_id)
-   end
+  def reservation_params
+    params.require(:reservation).permit(:date, :city, :course_id, :user_id)
+  end
 end
-
