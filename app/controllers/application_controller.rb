dac93db11a76a3a93b81ed76d9aa5edc19d57b28
sneeu-leeu <sa070197@gmail.com>
2022-06-20 20:43:22 +0200

    Fix: parameters for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
   end
 end
