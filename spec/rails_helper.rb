d2be37d8414347739d8eae4f08e67489b574f224
Benjamin Semah <benjaminsemah@gmail.com>
2022-06-22 21:29:37 +0000

    Fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -61,6 +61,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  # rubocop:disable Lint/ShadowingOuterLocalVariable
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       # Choose a test framework:
@@ -68,4 +69,5 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+  # rubocop:enable Lint/ShadowingOuterLocalVariable
 end
