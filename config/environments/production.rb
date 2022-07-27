df3e6961fd398f0ac69a64b7889dd21183d86890
Omar Salem <omarsalem0721@gmail.com>
2022-07-27 19:20:08 +0200

    Modified Gemfile.lock

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
